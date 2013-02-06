fb98a0ab02d111e8cb6d389919db40e19d75ee8e
qianwen <yellowfireflies@gmail.com>
2013-02-06 18:54:58 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitzy::Application.config.secret_token = 'ad639cb21c601277105343ad3ae8c11442d0f847e741ef000533518d2376c90b5de0e8bb2c4254f45b027d0fa3743b0a0047b256c82229f92ae72ac2c3d80bcf'
